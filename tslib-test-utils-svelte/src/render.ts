a9e54baeb5995fe91369fad4611ab5dd426073bb
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2022-01-07 13:47:36 +0100

    HOTFIX - Fixes `render` return type

--- tslib-test-utils-svelte/src/render.ts
+++ tslib-test-utils-svelte/src/render.ts
@@ -1,10 +1,13 @@
 import { tick } from 'svelte';
-import { render as testingRender } from '@testing-library/svelte';
+import {
+  render as testingRender,
+  RenderResult,
+} from '@testing-library/svelte';
 
 
 export async function render(
   ...args: Parameters<typeof testingRender>
-): Promise<ReturnType<typeof testingRender>> {
+): Promise<RenderResult> {
   const result = testingRender(...args);
   await tick();
   return result;
