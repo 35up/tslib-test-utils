49180b9ce2c72e78f690bd2190d692d7c9b17126
Fausto A. Guerrero <enjoythelive1@gmail.com>
2022-01-11 11:11:07 +0100

    UP-2154 - Completes letters set

--- tslib-test-utils/src/generators/char.ts
+++ tslib-test-utils/src/generators/char.ts
@@ -1,7 +1,7 @@
 import { integer } from './integer';
 
-export const UPPER_CASE_LETTERS = 'ABCDEFGHIJKMNOPQRSTUVWYZ'.split('');
-export const LOWER_CASE_LETTERS = 'abcdefghijkmnopqrstuvwyz'.split('');
+export const UPPER_CASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+export const LOWER_CASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('');
 export const NUMBERS = '1234567890'.split('');
 export const SYMBOLS = '!"§$%&/()=\'`#+*~-_.:,;<>|^°?'.split('');
 export const ALL = UPPER_CASE_LETTERS.concat(
