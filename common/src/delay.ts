80578d49fe3701f232df4518f015c6f9c9a25766
drafter500 <vzemtsov@yahoo.com>
2023-03-30 17:57:43 +0200

    UP-4374 - Fixes linter error

--- common/src/delay.ts
+++ common/src/delay.ts
@@ -1,7 +1,7 @@
 export function delayResolve<T>(ms: number, result: T): Promise<T> {
-  return new Promise(resolve => setTimeout(() => resolve(result), ms));
+  return new Promise((resolve) => { setTimeout(() => resolve(result), ms); });
 }
 
 export function delay(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => { setTimeout(resolve, ms); });
 }
